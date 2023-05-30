d732c611a1acd669d4d74885d650d10e5dd1ccee
helena <helenapaulasova@hotmail.com>
2023-05-30 13:02:30 +0200

    sending GET requests refactoring: - read port from URL and use default if none specified - don't send '?' if no query specified in URL

--- src/step3/send_get_request.py
+++ src/step3/send_get_request.py
@@ -3,17 +3,27 @@ import urllib.parse
 
 
 async def send_get(url):
+    def is_empty(p):
+        is_none = p is None
+        is_empty = False
+        if type(p) is str:
+            is_empty = len(p) == 0
+        return is_none or is_empty
+
     url = urllib.parse.urlsplit(url)
     if url.scheme == 'https':
+        port = 443 if is_empty(url.port) else url.port
         reader, writer = await asyncio.open_connection(
-            url.hostname, 443, ssl=True)
+            url.hostname, port, ssl=True)
     else:
+        port = 80 if is_empty(url.port) else url.port
         reader, writer = await asyncio.open_connection(
-            url.hostname, 80)
+            url.hostname, port)
 
+    query = "" if is_empty(url.query) else "?" + url.query
     request = (
-        f"GET {url.path or '/'}{'?' + url.query or ''} HTTP/1.0\r\n" # request line
-        f"Host: {url.hostname}\r\n" # headers
+        f"GET {url.path or '/'}{query} HTTP/1.0\r\n" # request line
+        f"Host: {url.netloc}\r\n" # headers
         "\r\n" # there is no body - this is the end
     )
 
@@ -35,8 +45,10 @@ if __name__ == "__main__":
     http://hela-httpbin.fly.dev/status/404
     http://hela-httpbin.fly.dev/headers
     http://libsql.org/hrana-client-ts/
-    http://paulgraham.com
+    https://demo.chystat.com:8443    
     """
     asyncio.run(send_get(url))
+
+
 
 
